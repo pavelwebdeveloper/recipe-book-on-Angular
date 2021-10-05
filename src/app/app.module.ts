1893170c2fb4c2b8f2e97552e769041aa9056b92
Pavel Shvets <38735112+pavelwebdeveloper@users.noreply.github.com>
2021-10-05 11:14:31 +0300

    added a custom directive to make dropdowns work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.com
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { DropdownDirective } from './shared/dropdown.directive';
 /*import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
 import { WarningAlertComponent } from './warningalert/warningalert.component';
@@ -24,7 +25,8 @@ import { SuccessalertComponent } from './success-alert/successalert.component';*
     RecipeDetailComponent,
     RecipeItemComponent,
     ShoppingListComponent,
-    ShoppingEditComponent/*,
+    ShoppingEditComponent,
+    DropdownDirective/*,
     ServerComponent,
     ServersComponent,
     WarningAlertComponent,
