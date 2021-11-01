addd2faa5cd9852d13ecbfdd76390098dee1b0fb
Pavel Shvets <38735112+pavelwebdeveloper@users.noreply.github.com>
2021-11-01 14:18:16 +0200

    implementing template form in the shopping list component

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -28,6 +28,10 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
     );
   }
 
+  onEditItem(index: number){
+    this.slService.startedEditing.next(index);
+  }
+
   ngOnDestroy(): void {
     this.igChangeSub.unsubscribe();
   }
